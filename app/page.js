173d7887b1e6048c9183e24ddfdaf752014e4f1c
mcsavvy <davemcsavvii@gmail.com>
2023-09-20 12:06:52 +0000

    hot fix

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Hero from '@/components/landingPgComponents/hero'
 import {SearchBar} from '@/components/landingPgComponents/searchBar'
 import { LatestRecipes } from '@/components/landingPgComponents/latestRecipies'
 import { FreshFromCommunity } from '@/components/landingPgComponents/frshFrmCommunity'
-import { ExploreNigerianRecipies } from '@/components/landingPgComponents/ExploreNigerianRecipies'
+import { ExploreNigerianRecipies } from '@/components/landingPgComponents/exploreNigerianRecipies'
 import { Footer } from '@/components/footer'
 import { Navbar } from '@/components/navbar/NavBar'
 
