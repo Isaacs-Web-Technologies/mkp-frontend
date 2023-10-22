f63c4109c439b83f26dde8e3b17eada6a746b8dd
mcsavvy <davemcsavvii@gmail.com>
2023-10-22 02:49:22 +0000

    API Integration Fixes

--- components/User/chat/sidebar.jsx
+++ components/User/chat/sidebar.jsx
@@ -1,3 +1,5 @@
+import AxiosInstance from "@/components/axiosInstance";
+import { useEffect } from "react";
 import {
   AiOutlineMessage,
   AiOutlinePlus,
@@ -7,24 +9,66 @@ import {
 import { BiLinkExternal } from "react-icons/bi";
 import { FiMessageSquare } from "react-icons/fi";
 import { MdLogout } from "react-icons/md";
+import { useSelector, useDispatch } from 'react-redux';
+import { setThreads, openThread, startNewThread } from '@/redux/chatSlice';
+
+const ChatThread = ({ title, id }) => {
+  const dispatch = useDispatch();
+  return (
+    <a className="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer break-all hover:pr-4 group" onClick={() => {
+      AxiosInstance.get(`/chat/all?thread_id=${id}`).then(response => {
+        const chats = response.data.chats;
+        const messages = chats.map(chat => ({
+          content: chat.content,
+          id: chat.id,
+          sender: chat.chat_type == 'RESPONSE' ? 'ai' : 'user'
+        }));
+        dispatch(openThread({thread_id: id, messages: messages}))
+      })
+    }}>
+      <FiMessageSquare className="h-4 w-4" />
+      <div className="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">
+        {title}
+        <div className="absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#2A2B32]"></div>
+      </div>
+    </a>
+  )
+}
 
 const Sidebar = () => {
+  const threads = useSelector(state => state.chat.threads);
+  const dispatch = useDispatch();
+  console.log("threads value:", threads);
+
+  useEffect(() => {
+    console.log("threads value:", threads);
+    AxiosInstance.get(`/chat/threads`).then(response => {
+      if (response.status !== 200) throw new Error(response.statusText);
+      const threads = response.data.threads;
+      const allThreads = [{id: null, messages: [], title: "New Thread"}];
+      console.log("threads: ", threads);
+      dispatch(setThreads(threads.map(thread => ({
+          id: thread.id,
+          title: thread.title,
+          messages: []
+      }))));
+    });
+  }, []);
   return (
     <div className="scrollbar-trigger flex h-full w-full flex-1 items-start bg-primary border-white/20">
       <nav className="flex h-full flex-1 flex-col space-y-1 p-2">
-        <a className="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-pink-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-1 flex-shrink-0 border border-white/20">
+        <a className="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-pink-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-1 flex-shrink-0 border border-white/20"
+          onClick={() => {
+            dispatch(startNewThread());
+          }}>
           <AiOutlinePlus className="h-4 w-4" />
           New chat
         </a>
         <div className="flex-col flex-1 overflow-y-auto border-b border-white/20">
           <div className="flex flex-col gap-2 pb-2 text-gray-100 text-sm">
-            <a className="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer break-all hover:pr-4 group">
-              <FiMessageSquare className="h-4 w-4" />
-              <div className="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">
-                New conversation
-                <div className="absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#2A2B32]"></div>
-              </div>
-            </a>
+            {threads.filter(t => t.id !== null).map(thread => (
+              <ChatThread key={thread.id} title={thread.title} id={thread.id} />
+            ))}
           </div>
         </div>
         <a className="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
@@ -48,7 +92,7 @@ const Sidebar = () => {
           Get help
         </a>
         <a className="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm"
-        href="/">
+          href="/">
           <MdLogout className="h-4 w-4" />
           Log out
         </a>
--- redux/chatSlice.jsx
+++ redux/chatSlice.jsx
@@ -1,16 +1,42 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import AxiosInstance from '@/components/axiosInstance';
-
+import { store } from '@/redux/store';
 // Async thunk for sending a message to AI
 export const sendMessage = createAsyncThunk(
   'chat/sendMessage',
   async ( { message, thread_id },{ rejectWithValue }) => {
+    var payload = {query: message};
+    var isNewThread = true;
+    store.dispatch(chatSlice.actions.addQuery(message));
     try {
-      const response = await AxiosInstance.post(`/chat/`, { query: message });
+      if (thread_id !== null) {
+        payload.thread_id = thread_id;
+        isNewThread = false;
+      }
+      const response = await AxiosInstance.post(`/chat/`, payload);
       console.log("API response:", response);
-      if (response.status !== 201 && response.status !== 200) throw new Error(response.statusText);
+      if (response.status !== 201 && response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      thread_id = response.data.chat.thread_id;
+      // If this is a new thread, we need to update the thread_id
+      // in the redux store
+      if (isNewThread) {
+        store.dispatch(chatSlice.actions.startNewThread(thread_id));
+      }
       console.log("threadId: ", thread_id);
-      return {data: response.data, thread_id };
+
+      return {
+        query: {
+          content: message,
+          id: response.data.chat.previous_chat_id,
+        },
+        response: {
+          content: response.data.chat.content,
+          id: response.data.chat.id,
+        },
+        thread_id: thread_id,
+      };
     } catch (error) {
       return rejectWithValue(error.message);
     }
@@ -31,21 +57,59 @@ export const sendMessage = createAsyncThunk(
 //   }
 // );
 
+
 const chatSlice = createSlice({
   name: 'chat',
   initialState: {
     threads: [],
-    activeThreadId: [],
+    activeThreadId: null,
+    isNewThread: true,
     status: 'idle',
     error: null,
   },
   reducers: {
-    startNewThread: (state, action) => {
-      state.threads.push({
-        id: action.payload,
-        messages: []
-      });
+    setThreads: (state, action) => {
+      state.threads = action.payload;
+    },
+    setMessages: (state, action) => {
+      const thread = state.threads.find(t => t.id === action.payload.thread_id);
+      if (thread === undefined) return;
+      thread.messages = action.payload;
+    },
+    openThread: (state, action) => {
+      state.activeThreadId = action.payload.thread_id;
+      const thread = state.threads.find(t => t.id === action.payload.thread_id);
+      if (thread === undefined) return;
+      thread.messages = action.payload.messages;
+    },
+    updateActiveThreadId: (state, action) => {
+      console.log("updateActiveThreadId: ", action.payload);
       state.activeThreadId = action.payload;
+    },
+    addQuery: (state, action) => {
+      const activeThread = state.threads.find(t => t.id === state.activeThreadId);
+      if (activeThread) {
+        activeThread.messages.push({
+          content: action.payload,
+          sender: 'user',
+          id: `${Date.now()}`, // Placeholder ID
+        });
+      }
+    },
+    startNewThread: (state, action) => {
+      var thread = state.threads.find(t => t.id === null)
+      if (thread !== undefined) {
+        state.activeThreadId = thread.id;
+        state.messages = thread.messages;
+        return;
+      }
+      thread = {
+        id: null,
+        title: "New Thread",
+        messages: []
+      };
+      state.threads.splice(0, 0, thread);
+      state.activeThreadId = thread.id;
     },
     removeMessage: (state, action) => {
       const activeThread = state.threads.find(t => t.id === state.activeThreadId);
@@ -66,21 +130,20 @@ const chatSlice = createSlice({
         state.status = 'loading';
       })
       .addCase(sendMessage.fulfilled, (state, action) => {
-        const thread = state.threads.find(t => t.id === action.payload.thread_id);
-        if (thread) {
-          thread.messages.push({
-            content: action.payload.data.content,
-            sender: 'user',
-            id: action.payload.data.id
-          });
-        
-          // Let's assume the AI's response comes in the same payload 
-          thread.messages.push({
-            content: action.payload.data.aiResponse,
-            sender: 'ai',
-            id: `ai_${action.payload.data.id}`
-          });
+        const thread = state.threads.find(t => t.id === null || t.id === action.payload.thread_id);
+        if (thread === undefined) return;
+        if (thread.id === null) {
+          thread.id = action.payload.thread_id;
         }
+        
+        // replace placeholder ID with actual ID
+        const query = thread.messages[thread.messages.length - 1];
+        query.id = action.payload.query.id;
+        thread.messages.push({
+          content: action.payload.response.content,
+          sender: 'ai',
+          id: action.payload.response.id,
+        });
       })
       .addCase(sendMessage.rejected, (state, action) => {
         state.status = 'failed';
@@ -94,6 +157,9 @@ const chatSlice = createSlice({
   // });
 },
 });
-export const { startNewThread, removeMessage, deleteThread } = chatSlice.actions; // Exporting actions
-
+export const {
+  startNewThread, removeMessage, deleteThread,
+  setThreads, openThread, updateActiveThreadId,
+  setMessages
+} = chatSlice.actions; // Exporting actions
 export default chatSlice.reducer;
