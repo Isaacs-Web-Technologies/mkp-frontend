29a9bc3f34e0b09e28774125528a5c189ad6f146
mcsavvy <davemcsavvii@gmail.com>
2023-10-23 02:31:52 +0000

    feat(redux): enabled devtools

--- redux/store.jsx
+++ redux/store.jsx
@@ -4,5 +4,6 @@ import chatReducer from '@/redux/chatSlice';
 export const store = configureStore({
   reducer: {
     chat: chatReducer
-  }
+  },
+  devTools: process.env.NODE_ENV !== 'production',
 });
