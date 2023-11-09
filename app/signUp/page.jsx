2f02ee1d41a0cea5f391160b0f605ba4ec5b4762
lilyflowr <ibadiela@gmail.com>
2023-11-09 07:21:46 +0100

    updated signin and signup ui

--- app/signUp/page.jsx
+++ app/signUp/page.jsx
@@ -14,62 +14,11 @@ import Link from "next/link";
 import { resolve } from "styled-jsx/css";
 import { performSignUp, performLogin, useLoggedInUser } from "@/components/auth";
 
-// validating function
-function validateInput(firstName, lastName, email, password, cpassword) {
-
-  if (!firstName.match(/^[a-z0-9_-]+$/gi)) {
-    toast.error("username must only contain letters, numbers, underscores and hyphens")
-    return false
-  }
-  if (!lastName.match(/^[a-z0-9_-]+$/gi)) {
-    toast.error("username must only contain letters, numbers, underscores and hyphens")
-    return false
-  }
-  if (firstName.search(/[a-z]/gi) == -1) {
-    toast.error("firstname must contain at least one letter")
-    return (false)
-  }
-  if (lastName.search(/[a-z]/gi) == -1) {
-    toast.error("lastname must contain at least one letter")
-    return (false)
-  }
-  if (email.search(/@/) == -1) {
-    toast.error("email must be valid")
-    return (false)
-  }
-  if (password !== cpassword) {
-    toast.error("passwords don't match")
-    return (false)
-  }
-  if (password.length < 8) {
-    toast.error("password must be at least 8 characters long")
-    return (false)
-  }
-  if (password.search(/[0-9]/) == -1) {
-    toast.error("password must contain digit")
-    return (false)
-  }
-  if (password.search(/[A-Z]/) == -1) {
-    toast.error("password must contain uppercase letters")
-    return (false)
-  }
-  if (password.search(/[a-z]/) == -1) {
-    toast.error("password must contain lowercase letters")
-    return (false)
-  }
-  if (password.search(/[!"#$%&\\'()*+,-.\/:;<=>?@\[\]^_`\{\|\}~]/) == -1) {
-    toast.error("password must contain symbols")
-    return (false)
-  }
-  return (true)
-};
 
 
 
 const SignUpPage = () => {
   const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  // const [username, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [cpassword, setCPassword] = useState("");
@@ -79,13 +28,6 @@ const SignUpPage = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-
-    // validate input fields
-    // if (!validateInput(firstName,lastName, email, password, cpassword)) {
-    //   console.error("Could not validate inputs.")
-    //   return;
-    // }
     if (password !== cpassword) {
       toast.error("passwords don't match")
       return (false)
@@ -113,116 +55,86 @@ const SignUpPage = () => {
 
 
   return (
-    <>
-      {/* heading */}
-      <section className="  lg:bg-primary bg-white  h-[58rem] ">
-        <div className="w-[100rem] h-[58rem] px-16 left-[574px] top-0 absolute bg-white rounded-tl-3xl rounded-bl-3x" />
-
-
-        {/* food image */}
-        <div className="hidden md:block mt-0 mb-0">
-          <p className="my-0 relative left-[21.13rem] top-[2rem] font-medium font-galada text-white ">My
-            <br />Kitchen
-            <br />Power</p>
-          <div className="relative my-0 w-[16.38rem] h-[17.06rem] top-[7.06rem] left-[5.13rem] rounded-[50%] bg-gray [filter:blur(100px)]  " />
-          <div className="absolute my-0 top-[10.06rem] left-[7.61rem] ">
-            <Image
-              className="w-full h-auto md:w-full md:h-full object-cover"
-              alt=""
-              src={MkpSignupImg}
-              width={550}
-              height={550}
-            />
-            <h3 className="relative text-[1rem] mt-10 text-center font-medium  text-white">Cook Like Never Before...</h3>
-          </div>
+     <>
+    <div className="flex flex-col md:flex-row min-h-screen bg bg-primary">
+      <div className="flex flex-1  items-center justify-center m-0 px-4">
+        <div className="hidden md:block">
+        <Link href="https://mykitchenpower.com">
+          <h3 className="mt-4 text-left text-white text-xl">My Kitchen Power</h3>
+          </Link>
+          <Image 
+            src={MkpSignupImg}
+            alt="Cooked meal"
+            width={400}
+            height={400}
+            className="rounded-full"
+          />
+          <h3 className="mt-4 text-left text-white text-xl">Cook Like Never Before...</h3>
         </div>
-
-        {/* email input div */}
-        <div className="max-w-md mx-auto text-center  lg:top-0 lg:absolute lg:left-[800px] justify-content-center d-flex ">
-
-          <div className=" top-[6.44rem] justify-content-center  text-left text-black">
-            <h2 className=" text-center  ">Create your account</h2>
-            <p className="text-center">Enter your personal details to start
-              <br /> your journey with us</p>
+      </div>
+      <div className="flex-1 flex items-center justify-center px-4 min-h-screen">
+      <div className="w-full max-w-md">  
+     <div className="bg-white p-10  mx-0 border border-gray-300 rounded-lg shadow-md">
+     <h2 className="mb-4 text-2xl font-bold text-center">Create your account</h2>
+      
+          <form onSubmit={handleSubmit}>
+          <div className="space-y-4 pr-4">
+                <div>
+                <label htmlFor="firstname" className="sr-only">Email</label>
+                <input
+                  type="firstName"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="Enter your name"
+                  required
+                />
+              </div>
+              <div>
+              <label htmlFor="email" className="sr-only">Email</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="Enter your email"
+                  required
+                />
+              </div>
+              <div>
+              <label htmlFor="password" className="sr-only">Password</label>
+              {/* Password Input */}
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  id="password"
+                  name="password"
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="*********"
+                  required
+                />
+              </div>
+             <div>
+              {/*Confirm  Password Input */}
+              <label htmlFor="confirm-password" className="sr-only">Confirm Password</label>
+                <input
+                  type="password"
+                  value={cpassword}
+                  onChange={(e) => setCPassword(e.target.value)}
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="***********"
+                  required
+                />
+              </div>
           </div>
 
-          {/* Form for collecting email */}
-          <form onSubmit={handleSubmit}>
-            {/* firstname input */}
-            <div className="w-full  lg:px-5 py-1 mb-5 mt-5 border-[1px] border-solid border-primary">
-              <input
-                type="firstName"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                className="w-full   py-2 bg-transparent border-none outline-none "
-                placeholder="Enter your name"
-                required
-              />
-            </div>
-            {/* lastname input */}
-            {/* <div className="w-full  lg:px-5 py-1 mb-5 mt-5 border-[1px] border-solid border-primary">
-              <input
-                type="lastName"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                className="w-full   py-2 bg-transparent border-none outline-none "
-                placeholder="Enter your last name"
-                required
-              />
-            </div> */}
-            {/* username input */}
-            {/* <div className="w-full  lg:px-5 py-1 mb-5 mt-5 border-[1px] border-solid border-primary">
-              <input
-                type="username"
-                value={username}
-                onChange={(e) => setUserName(e.target.value)}
-                className="w-full  py-2 bg-transparent border-none outline-none "
-                placeholder="Enter your username"
-              />
-            </div> */}
-            {/* email input */}
-            <div className="w-full  lg:px-5 py-1 mb-5 mt-5 border-[1px] border-solid border-primary">
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full   py-2 bg-transparent border-none outline-none "
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-            {/* Password Input */}
-            <div className="w-full  lg:px-5 py-1 mb-5 mt-5 border-[1px] border-solid border-primary">
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                name="password"
-                className="w-full   py-2 bg-transparent border-none outline-none"
-                placeholder="*********"
-                required
-              />
-            </div>
-
-            {/*Confirm  Password Input */}
-            <div className="w-full  lg:px-5 py-1 mb-5 mt-5 border-[1px] border-solid border-primary">
-              <input
-                type="password"
-                value={cpassword}
-                onChange={(e) => setCPassword(e.target.value)}
-                id="confirmPassword"
-                name="confirmPassword"
-                className="w-full   py-2 bg-transparent border-none outline-none"
-                placeholder="***********"
-                required
-              />
-            </div>
-
-            <div className="w-full h-full lg:px-[22px]  mb-5 mt-5 bg-primary  flex justify-center  ">
+            <div className="mt-6">
               <button
                 type="submit"
-                className="bg-transparent border-none py-[25.96px]  outline-none text-white "
+                className="w-full p-3 bg-primary text-white rounded-md hover:bg-orange-600 transition"
                 onKeyDown={e => {
                   if (e.key === "Enter" && !e.shiftKey) {
                     handleSubmit(e);
@@ -235,47 +147,17 @@ const SignUpPage = () => {
             </div>
 
           </form>
-
-          {/*  */}
-          <p className=" text-center">Already have an account? <Link href="./signIn" className="underline text-blue-600">Signin</Link></p>
-          <div className="justify-content-center  inline-block">
-            <div className=" box-border w-[15.63rem] h-[0.06rem]  border-solid border-black" />
-            <div className=" box-border w-[15.63rem] h-[0.06rem]  border-solid border-black" />
-            <h3 className="  ">OR</h3>
-          </div>
-
-          {/*  */}
-          <div className="grid  place-items-center ">
-
-            <div className="flex items-center">
-              <Image className=" w-[1.88rem] h-[1.88rem] overflow-hidden"
-                alt="" src={Apple}
-                width={30}
-                height={30} />
-              <h3 className="">Continue with Apple</h3>
+          <p className="mt-4 text-center">
+            Already have an account? 
+            <Link href="./signIn" 
+            className="hover:underline text-blue-600">Signin
+            </Link>
+          </p>
             </div>
-
-            <div className="flex items-center">
-              <Image className=" w-[1.88rem] h-[1.88rem] overflow-hidden"
-                alt=""
-                src={Microsoft}
-                width={30}
-                height={30} />
-              <h3 className="">Continue with Microsoft Account</h3>
-            </div>
-
-            <div className="flex items-center">
-              <Image className=" w-[1.88rem] h-[1.88rem] overflow-hidden"
-                alt=""
-                src={GoogleIcon}
-                width={30}
-                height={30} />
-              <h3 className="">Continue with Google</h3>
-            </div>
-          </div>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
+     </>
   );
-};
+}
 export default SignUpPage;
