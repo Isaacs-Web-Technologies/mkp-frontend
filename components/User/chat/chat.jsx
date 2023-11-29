54eb5519d41c5538e6e42f1341a1de2791dc5204
lilyflowr <ibadiela@gmail.com>
2023-11-29 17:38:12 +0100

    worked on newchat display

--- components/User/chat/chat.jsx
+++ components/User/chat/chat.jsx
@@ -7,7 +7,7 @@ import Message from "@/components/User/chat/messages";
 import { useDispatch, useSelector } from 'react-redux';
 import { sendMessage, startNewThread } from '@/redux/chatSlice';
 import { ChatThread } from "./sidebar";
-
+// import {MKPLogo} from "@/public/images/mkpLogo.png"
 
 
 const Chat = (props) => {
@@ -40,10 +40,16 @@ const Chat = (props) => {
    
   };
     
+  const handleCardClick =(messageText)=> {
+    setmessage(messageText);
+    dispatch(sendMessage({message: messageText,thread_id: activeThreadId }));
+    setmessage('');
+  };
+
   return (
-    <div className="flex max-w-full flex-1 h-100dvh flex-col container  border border-black/10 dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)] ">
+    <div className="flex max-w-full flex-1 h-100vh flex-col container  border border-black/10 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)]">
       {/* hamburger menu for smaller screens and new chat */}
-      <div className="sticky top-0 z-10 flex items-center border-b  pl-1 pt-1 text-gray-200 sm:pl-3 md:hidden">
+      <div className="sticky top-0 z-10 flex items-center border-b  pl-1 pt-1 text-white-200 sm:pl-3 md:hidden">
         <button
           type="button"
           className="-ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md bg-primary hover:bg-primary/10 cursor-pointer  focus:outline-none focus:ring-2 border-none"
@@ -73,19 +79,44 @@ const Chat = (props) => {
                 <Message key={message.id} message={{role: message.sender, content: message.content}} />
             ))
         ) : (
-            <div className="px-3 pt-2 pb-3 text-center text-xs md:px-4 md:pt-3 md:pb-6"> 
-             <span>
-                <h1 className="">
-                    My Kitchen Power
-                </h1>
+         
+          <div>
+            <div className="px-3  justify-center pt-2 pb-3 text-center text-xs md:px-4 md:pt-3 md:pb-6">  
+          {/* <div className="flex justify-center">
+                    <img
+                        src="/public/images/mkpLogo.png"
+                        alt="MKP Logo" 
+                       className="w-10 h-10 floatingImage"
+                    />
+                </div> */}
+              <span>
+                  <h1>My Kitchen Power</h1>
               </span>
-            </div>        
+              <span>
+                  <p>Your kitchen assistant at your service</p>
+              </span>
+              <div className="cardContainer">
+              <div className="card" onClick={() => handleCardClick("How to make pizza?")}>
+                  <p className="typewriter">How to make pizza?</p>
+              </div>
+              <div className="card" onClick={() => handleCardClick("What to cook eba?")}>
+                  <p className="typewriter">What to cook eba?</p>
+              </div>
+              <div className="card" onClick={()=> handleCardClick("Give me kid-friendly snacks")} >
+                  <p className="typewriter">Give me kid-friendly snacks</p>
+              </div>
+              <div className="card" onClick={()=> handleCardClick("How do I cook banga soup?")}>
+                  <p className="typewriter">How do I cook banga soup?</p>
+              </div>
+          </div>
+           </div>
+           </div>
+          
+     
         )}
-        {/* <div className="flex flex-col items-center text-sm">
-      </div> */}
     </div>
        {/* input area */}
-       <div className=" bottom-0  w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
+       <div className=" bottom-0  w-full border-t md:border-t-0  md:border-transparent  md:bg-vert-light-gradient bg-white md:!bg-transparent pt-2">
           <form onSubmit={handleSubmit} className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
             <div className="relative flex flex-col h-full flex-1 items-stretch md:flex-col">
               {errorMessage ? (
@@ -95,18 +126,13 @@ const Chat = (props) => {
                   </div>
                 </div>
               ) : null}
-              <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-gray dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+              <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-white bg-primary  text-white  rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.10)]">
                 <textarea
                   value={message}
                   tabIndex={0}
                   data-id="root"
-                  style={{
-                    height: "28px",
-                    maxHeight: "200px",
-                    overflowY: "hidden",
-                  }}
-                  placeholder="Send a message..."
-                  className="m-0 w-full resize-none text-white border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0"
+                  placeholder="hello what do you want to cook today?"
+                  className="ChatTextArea"
                   onChange={e => setmessage(e.target.value)}
                   onKeyDown={e => {
                     if (e.key === "Enter" && !e.shiftKey) {
@@ -118,9 +144,9 @@ const Chat = (props) => {
 
                 <button
                   onClick={handleSubmit}
-                  className="absolute p-1 rounded-md bottom-1.5 md:bottom-2.5 bg-transparent disabled:bg-gray-500 right-1 md:right-2 disabled:opacity-40"
+                  className="sendButton"
                 >
-                  <FiSend className="h-4 w-4 mr-1 text-white " />
+                  <FiSend className="h-4 w-4" />
                 </button>
               </div>
             </div>
@@ -140,3 +166,5 @@ const Chat = (props) => {
 };
 
 export default Chat;
+
+
