73ff01587c081d2ea52d4b86a39cb9315bf0c9f1
lilyflowr <ibadiela@gmail.com>
2024-05-20 18:09:51 +0100

    added monetag and google adsense

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import Script from 'next/script'
 import { Toaster } from 'react-hot-toast';
-import { Providers } from '@/redux/provider'; // Assuming your Redux provider is here
+import { Providers } from '@/redux/provider'; 
 import '../public/styles/globals.css';
+import ServiceWorkerRegister from '@/components/ServiceWorkerRegister';
 
 export default function RootLayout({ children }) {
   const GTM_ID = 'GTM-PL87LPRW'; 
 
   return (
     <html lang="en">
-      <Script id="google-tag-manager" 
-      strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-    </Script>
+<head>
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
+        {/* Google AdSense Script */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6822803825285284"
+          crossorigin="anonymous"
+        ></Script>
+      </head>
       <body>
         <main className="app">
           <Toaster />
           <Providers>
+            <ServiceWorkerRegister />
             {children}
           </Providers>
         </main>
-      
 
-        </body>
-        {/* <noscript
-        dangerouslySetInnerHTML={{
-        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-        }} 
-    /> */}
+        {/* Google Tag Manager NoScript */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+      </body>
     </html>
   );
-};
+}
